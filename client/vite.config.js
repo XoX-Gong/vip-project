8e5fae8fa17cdac7ca0c7f3c9fc924df4b7a3217
wd2010 <wendan516033593@163.com>
2021-10-25 01:10:51 +0800

    feat: update

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,18 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import rowLoader from 'vite-raw-plugin'
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, '')
-      },
-    }
-  },
+  emptyOutDir: true,
   plugins: [vue()],
   build: {
-    sourcemap: true,
     outDir: '../extension/popup'
   }
 })
